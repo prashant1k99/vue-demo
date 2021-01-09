49885ea1ace32daa7fb1042192c6237b512acceb
Prashant Singh <41435180+prashant1k99@users.noreply.github.com>
2021-01-09 19:58:09 +0530

    added empty comment field

--- src/components/Comments.vue
+++ src/components/Comments.vue
@@ -1,11 +1,17 @@
 <template>
   <div>
     <h3> Comments </h3>
-    <v-list-item v-for="comment of comments" :key="comment.id">
-      <v-list-item-content>
-        <v-list-item-title>{{comment.body}}</v-list-item-title>
-      </v-list-item-content>
-    </v-list-item>
+    <div v-if="comments.length">
+      <v-list-item v-for="comment of comments" :key="comment.id">
+        <v-list-item-avatar>
+          <v-img :src="getAvatar(comment.id)"></v-img>
+        </v-list-item-avatar>
+        <v-list-item-content>
+          <v-list-item-title>{{comment.body}}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </div>
+    <p v-else> No Comments Found </p>
     <hr />
     <br />
     <CreateComment />
@@ -25,6 +31,11 @@ export default {
     comments () {
       // eslint-disable-next-line eqeqeq
       return this.$store.state.comment.comments.filter(comment => comment.postId == this.$route.params.id)
+    }
+  },
+  methods: {
+    getAvatar (id) {
+      return `https://cdn.vuetifyjs.com/images/lists/${id}.jpg`
     }
   }
 }
